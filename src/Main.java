8bdf70f7bf0a3f9aa3c60ed4f3fa06d92884787c
ruslanzverkov <createmed@mail.ru>
2022-07-16 16:38:49 +0300

    HomeworkEnd

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,39 @@ public class Main {
         }
 //        Четвертая задача
         short deliveryDistance=95;
+        if (deliveryDistance<=20){
+            System.out.println("Потребуеться дней"+1);
+        }else if (deliveryDistance>20&&deliveryDistance<=60){
+            System.out.println("Потребуеться дней"+2);
+        }else if (deliveryDistance>60&&deliveryDistance<=100){
+            System.out.println("Потребуеться дней"+3);
+        }
+//        Пятая задача
+        byte monthNumber = 13;
+        switch (monthNumber){
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца несуществует");
+        }
 
 
     }
